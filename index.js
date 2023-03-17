7add2d7d3080c86fd93dfcbd7409a3274608e66a
Oana <oananiste@gmail.com>
2023-03-17 13:50:56 +0100

    Add local stoarge functionality to leads and add functional delete button

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
 let myLeads = [];
 const inputEl = document.getElementById("input-el");
-const inputBtn = document.getElementById("input-btn"); //const and let are two types of variables; const is short for "constant"; this is a variable that is CONSTANT and its value cannot be reassigned. !!! If possible, use by default const. If not, use let. !!!
+const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
-inputBtn.addEventListener("click", function () {
-  myLeads.push(inputEl.value);
-  inputEl.value = "";
-  renderLeads();
-});
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  render(myLeads);
+}
 
-function renderLeads() {
+function render(leads) {
   let listItems = "";
-  for (let i = 0; i < myLeads.length; i++) {
+  for (let i = 0; i < leads.length; i++) {
     listItems += `
       <li>
-        <a target='_blank' href='${myLeads[i]}'> 
-          ${myLeads[i]}
+        <a target='_blank' href='${leads[i]}'> 
+          ${leads[i]}
         </a>
       </li>`;
   }
   ulEl.innerHTML = listItems;
 }
+
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+});
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
+});
